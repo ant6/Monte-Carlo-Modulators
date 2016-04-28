7d17f5cb5cb5cd61cb594be95b5bc5c39f55e8f5
ant6 <ant6ant@gmail.com>
2016-04-28 17:07:37 +0200

    lottery skeleton

---
+++ mcm/lottery.py
@@ -0,0 +1,12 @@
+#TODO: :)
+
+
+def random_peak_with_position(begin, end, peaks, n):
+    """
+    Return n pairs (random peak from peaks, random number from <begin, end>)
+    :param begin:
+    :param end:
+    :param peaks: this should be a list
+    :return: list of pairs?
+    """
+    pass
