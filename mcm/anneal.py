04481aae2392e7dbd2acaa0148fea5bf75c5fe41
ant6 <ant6ant@gmail.com>
2016-06-03 11:04:06 +0200

    Showcase version

--- mcm/anneal.py
+++ mcm/anneal.py
@@ -9,6 +9,33 @@ import random
 import math
 
 
+best_qual = 4000000000
+plots = {}
+
+def prepare_plot(norm=True, begin=None, end=None):
+    plt.ion()
+    plt.xlabel("Depth in water [mm]")
+    plt.ylabel("Relative dose")
+    if norm:
+        plt.ylim([0, 1.2])
+    if begin and end:
+        plt.plot([begin, begin], [0, 1.2])
+        plt.plot([end, end], [0, 1.2])
+        plt.grid(True)
+    plt.show()
+
+
+def plot_one_peak(name, peak, title=None, format=''):
+    if title:
+        plt.title(title)
+    try:
+        plots[name].remove()
+    except KeyError:
+        pass
+    plots[name], = plt.plot(peak[0], peak[1], format)
+    plt.pause(0.001)
+
+
 def _plot_one_peak(peak, title=None):
     if title:
         plt.title(title)
@@ -103,7 +130,7 @@ if __name__ == '__main__':
 
     t_start = time.time()
     k = 0
-    k_end = 1000
+    k_end = 1000000
     r = 0
     begin = 5
     end = 15
@@ -125,8 +152,17 @@ if __name__ == '__main__':
         else:
             results.append((k, e1))
 
+        qual = p.quality()
+        plot_one_peak('current', p.sum_peak(), format='k')
+
+        if qual < best_qual:
+            plot_one_peak('best', p.sum_peak(), title="Quality %.4f (better by %.4f)" % (qual, best_qual - qual), format='r')
+            best_qual = qual
+
         if k % 100 == 0:
             print("Step: %d, sample score: %.2f" % (k, e1))
+            # _plot_one_peak(p.sum_peak())
+
 
     # print time elapsed
     print("reverted %s times (ran %s times)" % (r, k_end))
@@ -138,4 +174,3 @@ if __name__ == '__main__':
     import pickle
     with open("sa.p", "w+b") as f:
         pickle.dump(results, f)
-    # _plot_one_peak(p.sum_peak())
--- mcm/read_n_draw.py
+++ mcm/read_n_draw.py
@@ -9,51 +9,55 @@ from os.path import join
 class BullszitError(Exception):
     pass
 
+a = False
 # Annealing
 # data_an = pickle.load(open(join("..", "res", "1464640516.0188088.p"), "rb"))  # 10k
-data_an = pickle.load(open(join("..", "res", "1464643304.7194686.p"), "rb"))  # 1k
+# data_an = pickle.load(open(join("..", "res", "1464643304.7194686.p"), "rb"))  # 1k
 
+m = True
 # Monte Carlo
-data_mc = pickle.load(open(join("..", "res", "1464709910.6499007.p"), "rb"))  # 1k
+# data_mc = pickle.load(open(join("..", "res", "1464709910.6499007.p"), "rb"))  # 1k
 # data_mc = pickle.load(open(join("..", "res", "1464710211.0149205.p"), "rb"))  # 10k
 # data_mc = pickle.load(open(join("..", "res", "1464710544.9451396.p"), "rb"))  # better 10k
+data_mc = pickle.load(open(join("..", "res", "1464865217.9277039.p"), "rb"))  # 100k
 
-xa, ya = np.array(data_an).T
-xm, ym = np.array(data_mc).T
-
-if len(xa) != len(xm):
-    raise BullszitError("Domains are different!")
+if a:
+    xa, ya = np.array(data_an).T
+if m:
+    xm, ym = np.array(data_mc).T
 
 hands = []
-if data_an:
-    ya2 = []
-    best = ya[0]
-    for e in ya:
-        if e < best:
-            best = e
-        ya2.append(best)
-    a1, = plt.plot(xa, ya, label="SA score")
-    a2, = plt.plot(xa, ya2, label="SA best")
-    hands.append(a1)
-    hands.append(a2)
+if a:
+    if data_an:
+        ya2 = []
+        best = ya[0]
+        for e in ya:
+            if e < best:
+                best = e
+            ya2.append(best)
+        a1, = plt.plot(xa, ya, label="SA score")
+        a2, = plt.plot(xa, ya2, label="SA best")
+        hands.append(a1)
+        hands.append(a2)
 
-if data_mc:
-    ym2 = []
-    best = ym[0]
-    for e in ym:
-        if e < best:
-            best = e
-        ym2.append(best)
-    # m1, = plt.plot(xm, ym, label="MC score")
-    m2, = plt.plot(xm, ym2, label="MC best")
-    # hands.append(m1)
-    hands.append(m2)
+if m:
+    if data_mc:
+        ym2 = []
+        best = ym[0]
+        for e in ym:
+            if e < best:
+                best = e
+            ym2.append(best)
+        # m1, = plt.plot(xm, ym, label="MC score")
+        m2, = plt.plot(xm, ym2, label="MC best")
+        # hands.append(m1)
+        hands.append(m2)
 
 from scipy.signal import medfilt
-ym = medfilt(ym, kernel_size=21)
-
-m1, = plt.plot(xm, ym, label="MC score")
-hands.append(m1)
+if m:
+    ym = medfilt(ym, kernel_size=21)
+    m1, = plt.plot(xm, ym, label="MC score")
+    hands.append(m1)
 
 plt.xlabel("Time")
 plt.ylabel("Quality")
